ff27da60beafcca99ffa4663e19dae9c1ca03464
Raden Arsil Kusumah Dinata <arsilkusumah1234@gmail.com>
2026-09-15 10:15:48 +0700

    Setup Vercel deployment configuration

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
+// Arahkan folder storage & bootstrap cache ke /tmp agar writable di Vercel
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp');
+
 require __DIR__ . '/../public/index.php';
